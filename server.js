f3789701f07cac452de6991ec65006a82f477f55
Jessica Pavone <JessicaPavone24@yahoo.com>
2020-06-12 19:03:16 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname,  './public/homepage.html'));
+});
+
+
+app.listen(port, function(){
+    console.log("Express server listening on  port: " + port);
+});
+        
